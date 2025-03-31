642114c23407e1270ec9658d0eef933b790a3b55
Carlos Abea <carlosabea91@gmail.com>
2025-03-31 10:33:11 -0600

    Biblioetca agregada statitics

--- example1.py
+++ example1.py
@@ -1,5 +1,5 @@
 #leer x cantidad de edad y calcular la media
-
+import statistics as st
 class Edad:
     def __init__(self, edades):
         self.edades = edades
@@ -8,6 +8,8 @@ class Edad:
     def mostrar_media(self):
         media = self.calcular_media()
         return f"La media de las edades es: {media: .2f}"
+    def calcu_media(self):
+        return st.mean(self.edades)
     
 def main():
     edades = []
@@ -27,6 +29,7 @@ def main():
     else:
         edad_obj = Edad(edades)
         print(edad_obj.mostrar_media())
+        print(f"La media cualcula con statistics: {edad_obj.calcu_media():.2f}")
         
     
 
